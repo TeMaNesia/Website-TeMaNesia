8ac3a124141bab41c03f90ae854f6179d12a74f0
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2023-06-25 12:22:18 +0700

    revise

--- app.py
+++ app.py
@@ -1,33 +1,32 @@
 import os
-# import pyrebase
+import pyrebase
 import hashlib
 
 from flask import Flask, session, render_template, request, redirect
-from firebase_admin import credentials, firestore, initialize_app, auth
-
+# from firebase_admin import credentials, firestore, initialize_app, auth
 
 app = Flask(__name__)
+app.secret_key = 'secret'
 
-# config = {
-#     'apiKey': "AIzaSyBAFpFJ2I_tx6fEa2ZfLco4smdwofm_S8o",
-#     'authDomain': "temanesia-6feb4.firebaseapp.com",
-#     'projectId': "temanesia-6feb4",
-#     'storageBucket': "temanesia-6feb4.appspot.com",
-#     'messagingSenderId': "348340047680",
-#     'appId': "1:348340047680:web:834e74dc41adba5c5374a5",
-#     'measurementId': "G-G0C48QK7RF",
-#     'databaseURL': ''
-# }
+config = {
+    'apiKey': "AIzaSyBAFpFJ2I_tx6fEa2ZfLco4smdwofm_S8o",
+    'authDomain': "temanesia-6feb4.firebaseapp.com",
+    'projectId': "temanesia-6feb4",
+    'storageBucket': "temanesia-6feb4.appspot.com",
+    'messagingSenderId': "348340047680",
+    'appId': "1:348340047680:web:834e74dc41adba5c5374a5",
+    'measurementId': "G-G0C48QK7RF",
+    'databaseURL': ''
+}
 
-# firebase = pyrebase.initialize_app(config)
-# auth = firebase.auth()
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
 
+# cred = credentials.Certificate('key.json')
+# initialize_app(cred)
+# db = firestore.client()
 
-cred = credentials.Certificate('key.json')
-initialize_app(cred)
-db = firestore.client()
-
-users_collection = db.collection('users')
+# users_collection = db.collection('users')
 
 new_account = {}
 
@@ -43,8 +42,8 @@ def index():
         try:
             user = auth.sign_in_with_email_and_password(email, hashlib.md5(password.encode()).hexdigest())
             session['user'] = user['localId']
-            return render_template('dashboard.html', umessage=session['user'])
-        
+            return redirect('/')
+
         except:
             return render_template('login.html', umessage="Login gagal, kesalahan email atau password")
         
@@ -59,7 +58,6 @@ def register():
         new_account['sektor_lembaga'] = request.form.get('sektor')
         new_account['telepon_lembaga'] = request.form.get('telepon')
         new_account['alamat_lembaga'] = request.form.get('alamat')
-
         return redirect('/create_account')
     
     return render_template('register.html')
@@ -73,7 +71,6 @@ def create_account():
         if password == confirm:
             new_account['email'] = request.form.get('email')
             new_account['password'] = hashlib.md5(password.encode()).hexdigest()
-
             return redirect('/email_verification')
     
     return render_template('create_account.html')
@@ -81,14 +78,12 @@ def create_account():
 @app.route('/email_verification', methods=['POST', 'GET'])
 def email_verification():
     if request.method == 'POST':
-        # not done
-        # auth.send_email_verification(account['idToken'])
         code = request.form.get('code_confirmation')
-        true_code = ""
+        true_code = "123"
         if code == true_code: 
             user = auth.create_user_with_email_and_password(new_account['email'], new_account['password'])
-            users_collection.document(user['localId']).set(new_account)
-
+            auth.send_email_verification(user['idToken'])
+            # users_collection.document(user['localId']).set(new_account)
             return redirect('/')
         
     return render_template('email_verification.html')
@@ -96,7 +91,6 @@ def email_verification():
 @app.route('/logout')
 def logout():
     session.pop('user')
-
     return redirect('/')
 
 port = int(os.environ.get('PORT', 8080))
